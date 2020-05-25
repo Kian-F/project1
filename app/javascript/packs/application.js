a56fe9a2a7787462acd558eba369eb6b3684a0de
Kian <kian.info78@gmail.com>
2020-05-26 01:04:25 +1000

    newHome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import 'bootstrap/dist/js/bootstrap';
+
+
+import "bootstrap"
+import "../stylesheets/application"
+
+
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 
